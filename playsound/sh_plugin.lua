c75c5c394ce92118e74847a2319b903cd130cad3
Olegle <dobrin_k1@abv.bg>
2022-02-05 05:22:42 +0200

    Fix

--- playsound/sh_plugin.lua
+++ playsound/sh_plugin.lua
@@ -3,17 +3,13 @@ PLUGIN.description = "Adds /PlaySound command"
 PLUGIN.author = "Apsys"
 
 
-ix.command.Add("PlaySound",{
-	description = "Play a sound to all users.",
-	adminOnly = true,
-	OnRun = function(ply, cmd, args)
-		if (ply:IsSuperAdmin()) then
-			for k, v in pairs(player.GetAll()) do
-				v:ConCommand("stopsound")
-				v:ConCommand("play "..tostring(args[1]))
-			end
-		else
-			ply:Notify("You need to be Superadmin to use this!")
-		end
-	end
-})
+concommand.Add("playsound", function(ply, cmd, args)
+    if (ply:IsSuperAdmin()) then
+        for k, v in pairs(player.GetAll()) do
+            v:ConCommand("stopsound")
+            v:ConCommand("play "..tostring(args[1]))
+        end
+    else
+        print("You need to be Superadmin to use this!")
+    end
+end)
