272675d827d86c5d65b1f2baf8340c7333a3a764
eon <dobrin_k1@abv.bg>
2023-11-01 01:03:42 +0200

    Format a little

--- sh_discordadvert.lua
+++ sh_discordadvert.lua
@@ -30,14 +30,14 @@ if ( SERVER ) then
     if not ( timer.Exists("SchemaDiscordAdvertTimer") ) then
         timer.Create("SchemaDiscordAdvertTimer", math.random(120, 360), 0, function()
             if ( ix.config.Get("discordReminder", false) ) then
-                Schema:MakeAnnouncement("Please make sure to join our discord! "..PLUGIN.discord)
+                Schema:MakeAnnouncement("Please make sure to join our discord! " .. PLUGIN.discord)
             end
         end)
     else
         timer.Remove("SchemaDiscordAdvertTimer")
         timer.Create("SchemaDiscordAdvertTimer", math.random(120, 360), 0, function()
             if ( ix.config.Get("discordReminder", false) ) then
-                Schema:MakeAnnouncement("Please make sure to join our discord! "..PLUGIN.discord)
+                Schema:MakeAnnouncement("Please make sure to join our discord! " .. PLUGIN.discord)
             end
         end)
     end
@@ -45,7 +45,6 @@ else
     net.Receive("SchemaDiscordAdvert", function(len)
         local txt = net.ReadString() or ""
 
-
-        chat.AddText(ix.config.Get("color"), PLUGIN.serverName.." Announcement", color_white, ": "..txt)
+        chat.AddText(ix.config.Get("color"), PLUGIN.serverName .. " Announcement", color_white, ": " .. txt)
     end)
 end
