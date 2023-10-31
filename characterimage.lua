32d5850da4872a46a7ac3d3a30e2276c6ba5d38b
eon <dobrin_k1@abv.bg>
2023-11-01 01:00:27 +0200

    Create characterimage.lua

---
+++ characterimage.lua
@@ -0,0 +1,42 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "Character Menu Image"
+PLUGIN.author = "eon | bloodycop"
+PLUGIN.images = {
+    {"icon16/", Color(255, 255, 255)}, // replace with your material (ex: icon16/anchor.png).
+}
+
+if ( SERVER ) then
+    return
+end
+
+local randomImageID = 1
+
+if not ( timer.Exists("ix.gui.characterMenu.ImageSwitcher") ) then
+    timer.Create("ix.gui.characterMenu.ImageSwitcher", 10, 0, function()
+        if not ( ix.gui.characterMenu ) then
+            ErrorNoHalt("[ix] [plugin: characterimage]: characterMenu not registered.")
+            return
+        end
+
+        local newTable = table.Copy(PLUGIN.images)
+
+        table.remove(newTable, randomImageID) // Make sure the current image is removed from a randomized table.
+
+        randomImageID = math.random(1, #newTable)
+    end)
+end
+
+if ( ix.gui.characterMenu ) then
+    local oldFunc = ix.gui.characterMenu.Paint
+
+    if ( IsValid(ix.gui.characterMenu) ) then
+        ix.gui.characterMenu.Paint = function(s, w, h)
+            surface.SetDrawColor(PLUGIN.images[randomImageID][2] or color_white)
+            surface.SetMaterial(Material(PLUGIN.images[randomImageID][1]))
+            surface.DrawTexturedRect(0, 0, w, h)
+
+            oldFunc(s, w, h)
+        end
+    end
+end
