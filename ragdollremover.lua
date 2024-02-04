5a26c79d97d174b828d7c242eec57377fb004142
eon <dobrin_k1@abv.bg>
2024-02-04 17:36:56 +0200

    Update ragdollremover.lua

--- ragdollremover.lua
+++ ragdollremover.lua
@@ -1,32 +1,40 @@
 local PLUGIN = PLUGIN
 
 PLUGIN.name = "Ragdoll Remover"
-PLUGIN.author = "Skay"
+PLUGIN.author = "eon (bloodycop)"
 PLUGIN.description = "Removes all ragdolls found on the ground."
 
-ix.config.Add("ragdoll_remove_time", 60, "How long to wait before removing ragdolls.", nil, {
-   data = {min = 0, max = 120},
+ix.config.Add("ragdollRemoveTime", 60, "How long to wait before removing ragdolls.", function(iOld, iNew)
+   if ( timer.Exists("ix.RagdollRemoveTimer") ) then
+      timer.Adjust("ix.RagdollRemoveTimer", iNew, 0)
+   end
+end, {
+   data = {min = 0, max = 500},
    category = PLUGIN.name
 })
 
-ix.config.Add("ragdoll_remove_enabled", false, "Should ragdoll remover be enabled?", nil, {
-   category = PLUGIN.name
-})
+ix.config.Add("ragdollRemoveEnabled", false, "Should ragdoll remover be enabled?", function(bOld, bNew)
+   if ( bNew ) then
+      if ( timer.Exists("ix.RagdollRemoveTimer") ) then
+         return
+      end
 
-if ( SERVER ) then
-   function PLUGIN:Tick()
-      if ( ix.config.Get("ragdoll_remove_enabled") ) then
-         if not ( timer.Exists("ragdollremovetimer") ) then
-            timer.Create("ragdollremovetimer", ix.config.Get("ragdoll_remove_time", 60), 0, function()
-               for k, v in pairs(ents.FindByClass("prop_ragdoll")) do
-                  SafeRemoveEntity(v)
-               end
-            end)
+      timer.Create("ix.RagdollRemoveTimer", ix.config.Get("ragdollRemoveTime", 60), 0, function()
+         for k, v in ipairs(ents.FindByClass("prop_ragdoll")) do
+            v:Remove()
          end
-      else
-         if ( timer.Exists("ragdollremovetimer") ) then
-            timer.Remove("ragdollremovetimer")
+
+         if ( CLIENT ) then
+            for k, v in ipairs(ents.FindByClass("class C_ClientRagdoll")) do  
+               v:Remove()
+            end
          end
+      end)
+   else
+      if ( timer.Exists("ix.RagdollRemoveTimer") ) then
+         timer.Remove("ix.RagdollRemoveTimer")
       end
    end
-end
+end, {
+   category = PLUGIN.name
+})
